1939ec44812c509479e88a71283ce7eb11663116
akhmedov <akhmedov@stream.ru>
2015-08-04 18:20:44 +0300

    asyncio, bad version

--- my_server.py
+++ my_server.py
@@ -1,45 +1,35 @@
-# Tcp Chat server
+import asyncio
 
-import socket, select, os
-
-
-def broadcast_data(message):
-    for socket in CONNECTION_LIST:
-        if socket != server_socket:
-            try:
-                socket.send(message)
-            except:
-                socket.close()
+connections = []
 
 
-if __name__ == "__main__":
-    RECV_BUFFER = 4096
-    PORT = 8027
+@asyncio.coroutine
+def handle_connection(reader, writer):
+    peername = writer.get_extra_info('peername')
+    connections.append(writer)
+    print('%s connected!' % str(peername))
+    while True:
+        data = yield from asyncio.wait_for(reader.readline(), timeout=120)
+        if data:
+            for connection_writer in connections:
+                if writer != connection_writer:
+                    connection_writer.write(data)
+        else:
+            print('%s lost connection' % str(peername))
+            connections.remove(peername)
+            break
+    writer.close()
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(("0.0.0.0", PORT))
-    server_socket.listen(10)
 
-    CONNECTION_LIST = {server_socket: 'server', }
-
-    print "Chat started on port " + str(PORT)
-
+def main():
+    loop = asyncio.get_event_loop()
+    server_gen = asyncio.start_server(handle_connection, port=8001)
+    print('Server started at port 8001')
+    server = loop.run_until_complete(server_gen)
     try:
-        while 1:
-            read_sockets, write_sockets, error_sockets = select.select(CONNECTION_LIST, [], [])
-            for sock in read_sockets:
-                if sock == server_socket:
-                    sock_new, addr = server_socket.accept()
-                    sock_new.send('Your name: ')
-                    name = sock_new.recv(1024)[:-2]
-                    CONNECTION_LIST[sock_new] = name
-                    print "Client (%s, %s) connected" % addr
-
-                    broadcast_data("%s entered room\n" % name)
-                else:
-                    data = sock.recv(RECV_BUFFER)
-                    if data:
-                        broadcast_data("\r" + str(CONNECTION_LIST[sock]) + ': ' + data)
+        loop.run_forever()
     except KeyboardInterrupt:
-        server_socket.close()
+        pass
+    finally:
+        server.close()
+        loop.close()
